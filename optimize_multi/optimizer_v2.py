d852b48c7ce29876095f391349ba49a0d8992fb9
Je-Hyeon Ryu <96281428+Je-Hyeon@users.noreply.github.com>
2023-05-20 15:55:36 +0900

    2023-05-20 Edit

--- optimize_multi/optimizer_v2.py
+++ optimize_multi/optimizer_v2.py
@@ -28,15 +28,16 @@ def run_optimizer(obj_function, rtn_df:pd.DataFrame, spx_mask, start_year:str, e
     spx_mask: S&P500 mask 데이터프레임 (ETF 최적화 하는 경우에는 "None" 주면 된다)
     look_back_size : int (과거 얼마 동안의 주가를 보고 포트폴리오를 구성할 지, "days")
     max_ratio : float (개별 주식당 포트폴리오 최대 비율 제한) 
-    shrinkage_method : str -> ["None", "linear", "constant", "clipping"]
-    arg: float -> shrinkage method에 맞는 arg 를 주면 된다. (linear, clipping만 해당!)
+    shrinkage_method : str -> ["None", "linear", "constant", "clipping", "kmeans]
+    arg: float -> shrinkage method에 맞는 arg 를 주면 된다. (linear, clipping, kmeans만 해당!)
     '''
     
     constraints = ({'type': 'eq', 'fun': lambda x: np.sum(x) - 1})
     shrink = {"None": no_shrinkage,
               "linear":linear_shrinkage,
               "constant":constant_correlation_model,
-              "clipping":eigenvalue_clipping
+              "clipping":eigenvalue_clipping,
+              "kmeans":k_means_clustering
               }
     
     weight_df = pd.DataFrame(columns=rtn_df.columns) # weight를 담을 dataframe
@@ -59,16 +60,25 @@ def run_optimizer(obj_function, rtn_df:pd.DataFrame, spx_mask, start_year:str, e
             mean_return = rtn_lookback.mean()
             corr_matrix = rtn_lookback.corr() # corr_matrix를 추정하고 축소한 후에, optimizer에 넣기 전에 cov_matrix로 변환해야함
             
-            shrinked_corr_matrix = shrink[shrinkage_method](corr_matrix = corr_matrix, arg=arg) # Corr_matrix를 축소
+            if shrinkage_method == "kmeans":
+                shrinked_corr_matrix = shrink[shrinkage_method](corr_matrix = corr_matrix, arg=[arg, rtn_lookback])
+            else:
+                shrinked_corr_matrix = shrink[shrinkage_method](corr_matrix = corr_matrix, arg=arg) # Corr_matrix를 축소
+                
             cov_matrix = rtn_vol.dot(shrinked_corr_matrix).dot(rtn_vol) # corr matrix를 cov matrix로 변경
                         
             bounds = tuple((0, max_ratio) for _ in range(len(rtn_lookback.columns)))
             initial_weights = np.ones(len(rtn_lookback.columns)) / len(rtn_lookback.columns)
             
             # 최적화 수행
+            if obj_function == obj_sharpe:
+                args = (cov_matrix, mean_return,)
+            elif obj_function == obj_variance:
+                args = (cov_matrix, )
+            
             result = minimize(obj_function, 
                               initial_weights, 
-                              args=(cov_matrix, mean_return,),
+                              args=args,
                               method='SLSQP', 
                               constraints=constraints, 
                               bounds=bounds
--- optimize_multi/shrinkage_method.py
+++ optimize_multi/shrinkage_method.py
@@ -1,6 +1,7 @@
 import warnings
 import numpy as np
 import pandas as pd 
+from sklearn.cluster import KMeans
 
 warnings.simplefilter(action='ignore', category=RuntimeWarning)
 pd.options.mode.chained_assignment = None 
@@ -38,3 +39,103 @@ def eigenvalue_clipping(corr_matrix, arg):
     eigen_value_clipped = np.where(eigen_value >= k, eigen_value_bigger, eigen_value_otherwise)
     return eigen_vector @ np.diag(eigen_value_clipped) @ eigen_vector.T
 
+
+
+def k_means_clustering(corr_matrix, arg):
+    # Paper대로 T,N, 람다_+ 구현
+    
+    alpha = arg[0]
+    sp500 = arg[1]
+    
+    t = len(sp500.index)
+    n = len(sp500.columns)
+    q = n/t 
+    lambda_plus = 1 + 2*(np.sqrt(q)) + q
+    
+    # Cluster의 개수를 구하기(RMT 이론에 의해)
+    eigen_values = np.linalg.eigvalsh(corr_matrix)
+    k = (eigen_values > lambda_plus).sum()
+    
+
+    kmean = KMeans(n_clusters=k, 
+                   n_init=200,
+                   max_iter=1000)
+    kmean.fit(sp500.T)
+    label = kmean.labels_ #라벨의 순서는 cov_matrix의 (idx,col)순서와 동일하다
+    
+    # within corr 구하기
+    within_corr_dict = {}
+
+    for i in range(k): # i는 클러스터를 의미
+        mask = (label == i)
+        card_cluster = mask.sum()
+        cluster_corr = sp500.loc[:,mask].corr().values
+
+        with_in_cluster = (cluster_corr - np.diag(np.diag(cluster_corr))).sum() / (card_cluster * (card_cluster-1))
+        if np.isnan(with_in_cluster) == True:
+            with_in_cluster = 0
+        within_corr_dict[i] = with_in_cluster 
+        
+    # between corr은 pair로 나와야함
+    between_corr_dict = {}
+
+    for i in range(k): # i는 클러스터를 의미
+        mask_i = (label == i)
+        card_i = mask_i.sum()
+        stock_i = sp500.loc[:, mask_i]
+
+        for j in range(k): # 클러스터 j를 뽑고
+            if i == j:
+                continue
+            mask_j = (label == j)
+            card_j = mask_j.sum()
+            stock_j = sp500.loc[:, mask_j]
+
+            all_corr = sp500.loc[:, mask_i+mask_j].corr().values
+            all_corr_sum = (np.triu(all_corr) - np.diag(np.diag(all_corr))).sum()
+
+            inner_corr_i = sp500.loc[:, mask_i].corr().values
+            all_corr_sum_i = (np.triu(inner_corr_i) - np.diag(np.diag(inner_corr_i))).sum()        
+
+            inner_corr_j = sp500.loc[:, mask_j].corr().values
+            all_corr_sum_j = (np.triu(inner_corr_j) - np.diag(np.diag(inner_corr_j))).sum()   
+
+            final_corr = all_corr_sum - all_corr_sum_i - all_corr_sum_j    
+
+            between_cluster = final_corr / (2* card_i * card_j)
+            between_corr_dict[(i,j)] = between_cluster
+            
+     # Within Correlation으로 S를 (i,j) 원소에 채우기... (i,j는 하나의 클러스터에 포함됨...)
+    cor_matrix_cluster = pd.DataFrame(index=corr_matrix.index,
+                                      columns=corr_matrix.columns)
+    
+    for i in range(k): # i는 각 클러스터를 의미함
+        mask = (label == i)
+        within_corr = within_corr_dict[i] # 이 within_corr을 각 회사의 pair 자리에 채워야함
+        
+        # select the rows and columns corresponding to the True values
+        selected_rows = cor_matrix_cluster.loc[mask, :]
+        selected_cols = cor_matrix_cluster.loc[:, mask]
+        # fill in the selected values with a specific value 
+        selected_rows.loc[:, selected_cols.columns] = within_corr
+        selected_cols.loc[selected_rows.index, :] = within_corr
+        # update the original correlation matrix with the modified values
+        cor_matrix_cluster.loc[mask, :] = selected_rows
+        cor_matrix_cluster.loc[:, mask] = selected_cols
+    
+    np.fill_diagonal(cor_matrix_cluster.values, 1) # 대각 행렬에 1을 채운다   
+    
+    # Between corr으로 각 위치에 값을 채우기: 각 클러스터 p,q에서 pair에서 주식을 뽑고
+    ## 주식 i, j자리에 행렬을 between corr으로 채운다
+
+    for (p,q), between_corr in between_corr_dict.items(): # p,q는 클러스터를 의미함
+        mask_p = (label == p)
+        mask_q = (label == q)
+
+        for i,bol_i in enumerate(mask_p): # i,j는 각각 클러스터에서 기업의 bol값을 의미함
+            if bol_i:
+                for j, bol_j in enumerate(mask_q):
+                    if bol_j:
+                        cor_matrix_cluster.iloc[i,j] = between_corr
+                        
+    r = alpha * cor_matrix_cluster + (1-alpha) * corr_matrix
